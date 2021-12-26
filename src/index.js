a23d7975862bbd0d9373fb0e4e82c32dc222bed4
Maxim Zorin <maxim.zorin@dev.zodiac.tv>
2021-12-26 17:43:20 +0300

    feat: implement decode

--- src/index.js
+++ src/index.js
@@ -37,8 +37,32 @@ const MORSE_TABLE = {
     '-----':  '0',
 };
 
+const REVERSE_MORSE = Object.entries(MORSE_TABLE)
+    .reduce((X, [key, value]) => (X[value] = key, X), {});
+
+const CODES_REVERSE_MORSE = Object.entries(REVERSE_MORSE)
+    .reduce((X, [key, value]) => (X[key] = value.replace(/./g, c => ({'.': '10', '-': '11'})[c]).padStart(10, '0'), X), {});
+
+function encode(expr) {
+    return expr
+        .split(' ')
+        .map(word => word.replace(/./g, c => CODES_REVERSE_MORSE[c]))
+        .join('**********');
+}
+
+function decodeWord(word) {
+    return word
+        .match(/.{10}/g)
+        .map(ten => ten.replace(/.{2}/g, c => ({'00': '', '10': '.', '11': '-'})[c]))
+        .map(x => MORSE_TABLE[x])
+        .join('');
+}
+
 function decode(expr) {
-    // write your solution here
+    return expr
+        .split('**********')
+        .map(decodeWord)
+        .join(' ');
 }
 
 module.exports = {
